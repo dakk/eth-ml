cf9e2861f536963b931863b5b7efbc07da7ae64a
Your Name <you@example.com>
2014-08-25 18:15:03 +0200

    first commit

---
+++ src/config.ml
@@ -0,0 +1,10 @@
+(* Configuration and default values *)
+
+
+(* RPC *)
+let rpc_host = "localhost";;
+let rpc_port = 8080;;
+
+(* Proto *)
+let proto_port = 30303;;
+let proto_string = "Ethereum(ml)/0.0.1";;
